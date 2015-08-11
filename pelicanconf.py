c225f1f9195eb1f055920ef309eb0cb42cee769f
Anna D Lukasiak <adlukasiak@gmail.com>
2015-08-10 23:19:17 -0400

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'James Solomon'
+SITENAME = u'OpenJC Blog'
+SITEURL = ''
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('OpenJC', 'http://openjerseycity.org/'),
+          ('CodeForAmerica', 'http://codeforamerica.org/'),)
+
+# Social widget
+SOCIAL = (('@OpenJerseyCity', 'https://twitter.com/OpenJerseyCity'),
+          ('OpenJC FB', 'https://www.facebook.com/OpenJC'),
+          ('Meetup','http://meetup.com/Open-JC'),
+          ('GitHub','https://github.com/OpenJC'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
